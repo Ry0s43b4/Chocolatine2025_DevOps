d1e5a64a78a20f5283cd5f7e36213006616b2d41
Nicky Truong <nicky.truong@epitech.eu>
2025-03-20 14:15:19 +0100

    feat: Direcotry for Unit tests (needed for the next job to be pushed)

---
+++ tests_unit/tests_file.c
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2025
+** Tests
+** File description:
+** File needed for the unit tests of the project
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
